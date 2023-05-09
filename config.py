4dc96d45c180828eca4e239b6ca6d82e5ea95a76
Colten Moshier <cjmoshier99@gmail.com>
2023-05-08 21:00:04 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+"Central configuration"
+import os
+
+PHOTOS_BUCKET = os.environ['PHOTOS_BUCKET']
+FLASK_SECRET = "something-random"
+CLOUDFRONT_DOMAIN = "https://d3bp06f68ag03t.cloudfront.net"
+
+DATABASE_HOST = os.environ['DATABASE_HOST'] if 'DATABASE_HOST' in os.environ else None
+DATABASE_USER = os.environ['DATABASE_USER'] if 'DATABASE_USER' in os.environ else None
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD'] if 'DATABASE_PASSWORD' in os.environ else None
+DATABASE_DB_NAME = os.environ['DATABASE_DB_NAME'] if 'DATABASE_DB_NAME' in os.environ else None
